e349a297e7cfe3ebcdddc487dbd087c979b48f8b
Oliver Schmid <oliver.schmd@gmail.com>
2020-08-29 11:01:11 -0700

    Extract settings and cleanup

--- roam-random-page.js
+++ roam-random-page.js
@@ -1,35 +1,44 @@
-var randomButton = document.querySelector('#random-button');
-if (randomButton != null) {
-  randomButton.parentNode.removeChild(randomButton);
+// settings
+const title = 'RANDOM';
+const icon = 'bp3-icon bp3-icon-random icon';
+const shortcut = 82; // Alt+R
+const timeout = 500; // ms
+
+function addButton() {
+  // cleanup old versions of the button
+  var randomButton = document.querySelector('#random-button');
+  if (randomButton != null) {
+    randomButton.parentNode.removeChild(randomButton);
+  }
+  // create button
+  var template = document.createElement('template');
+  template.innerHTML = '<div id="random-button" class="log-button" href="#"><div class="flex-h-box" style="align-items:center;justify-content:space-between;"><span class="' + icon + '"></span>' + title + '</div></div>';
+  template.content.firstChild.onclick = goToRandomPage;
+  randomButton = template.content.firstChild;
+  
+  // insert button into sidebar
+  const starred = document.querySelector('.starred-pages-wrapper');
+  document.querySelector('.roam-sidebar-content').insertBefore(randomButton, starred);
 }
-function getEventHandlers(element) {
-  for (var prop in element) {
-    if (prop.includes('reactEventHandlers')) {
-      return element[prop];
+
+function addKeyboardShortcut() {
+  document.onkeyup = function(e) {
+    var key = e.which || e.keyCode;
+    if (e.altKey && key === shortcut) {
+      goToRandomPage(e);
     }
   }
 }
-function goToAllPages() {
-  document.querySelector('.bp3-icon-list').parentNode.parentNode.click();
-}
-function clickRandomPage(shift) {
-  // https://forum.roamresearch.com/t/what-would-be-your-top-3-tips-for-beginners/255/9
-  var allpages = document.querySelectorAll('div.rm-pages-title-col a');
-  var pageLink = allpages[Math.floor(Math.random()*allpages.length)];
-  getEventHandlers(pageLink).onClick({ shiftKey: shift });
-}
-function isAllPages() {
-  return location.hash.endsWith('/search');
-}
+
 function goToRandomPage(e) {
   if (isAllPages()) {
-    clickRandomPage(e.shiftKey);
+    clickRandomPageLink(e.shiftKey);
   } else if (e.shiftKey) {
     goToAllPages();
     setTimeout(function() {
-      clickRandomPage(e.shiftKey);
+      clickRandomPageLink(e.shiftKey);
       history.back();
-    }, 500);
+    }, timeout);
   } else {
     const pages = roamAlphaAPI.q('[ :find (pull ?e [:block/uid]) :where [?e :node/title]]');
     const page = pages[Math.floor(Math.random()*pages.length)];
@@ -38,17 +47,29 @@ function goToRandomPage(e) {
     location.assign('/#/app/' + db + '/page/' + uid);
   }
 }
-function createRandomButton() {
-  var template = document.createElement('template');
-  template.innerHTML = '<div id="random-button" class="log-button" href="#"><div class="flex-h-box" style="align-items:center;justify-content:space-between;"><span class="bp3-icon bp3-icon-random icon"></span>RANDOM</div></div>';
-  template.content.firstChild.onclick = goToRandomPage;
-  return template.content.firstChild;
+
+function isAllPages() {
+  return location.hash.endsWith('/search');
 }
-const starred = document.querySelector('.starred-pages-wrapper');
-document.querySelector('.roam-sidebar-content').insertBefore(createRandomButton(), starred);
-document.onkeyup = function(e) {
-  var key = e.which || e.keyCode;
-  if (e.altKey && key === 82 /* Alt+R */) {
-    goToRandomPage(e);
+
+function goToAllPages() {
+  document.querySelector('.bp3-icon-list').parentNode.parentNode.click();
+}
+
+function clickRandomPageLink(shift) {
+  // https://forum.roamresearch.com/t/what-would-be-your-top-3-tips-for-beginners/255/9
+  var allpages = document.querySelectorAll('div.rm-pages-title-col a');
+  var pageLink = allpages[Math.floor(Math.random()*allpages.length)];
+  getEventHandlers(pageLink).onClick({ shiftKey: shift });
+}
+
+function getEventHandlers(element) {
+  for (var prop in element) {
+    if (prop.includes('reactEventHandlers')) {
+      return element[prop];
+    }
   }
 }
+
+addButton();
+addKeyboardShortcut();
