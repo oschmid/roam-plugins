c3d40ea184942ab4e80a74585cdfe1cf4844be18
Oliver Schmid <oliver.schmd@gmail.com>
2020-08-30 10:15:49 -0700

    Namespace

--- left-expander-plugin.js
+++ left-expander-plugin.js
@@ -1,27 +1,31 @@
-function addHoverElement() {
-  var leftExpander = document.querySelector('#left-expander');
-  if (leftExpander != null) {
-    leftExpander.parentNode.removeChild(leftExpander);
+function leftExpanderPlugin() {
+  
+  function addHoverElement() {
+    var leftExpander = document.querySelector('#left-expander');
+    if (leftExpander != null) {
+      leftExpander.parentNode.removeChild(leftExpander);
+    }
+    leftExpander = document.createElement('span');
+    leftExpander.id = 'left-expander';
+    leftExpander.style.height = '100%';
+    leftExpander.style.width = '1em';
+    leftExpander.style.position = 'absolute';
+    leftExpander.addEventListener('mouseenter', function(e) {
+      const leftMenu = document.querySelector('.bp3-icon-menu');
+      if (!leftMenu) { return; }
+      getEventHandlers(leftMenu).onMouseEnter();
+    });
+    document.querySelector('.roam-body-main').prepend(leftExpander);
   }
-  leftExpander = document.createElement('span');
-  leftExpander.id = 'left-expander';
-  leftExpander.style.height = '100%';
-  leftExpander.style.width = '1em';
-  leftExpander.style.position = 'absolute';
-  leftExpander.addEventListener('mouseenter', function(e) {
-    const leftMenu = document.querySelector('.bp3-icon-menu');
-    if (!leftMenu) { return; }
-    getEventHandlers(leftMenu).onMouseEnter();
-  });
-  document.querySelector('.roam-body-main').prepend(leftExpander);
-}
 
-function getEventHandlers(element) {
-  for (var prop in element) {
-    if (prop.includes('reactEventHandlers')) {
-      return element[prop];
+  function getEventHandlers(element) {
+    for (var prop in element) {
+      if (prop.includes('reactEventHandlers')) {
+        return element[prop];
+      }
     }
   }
-}
 
-addHoverElement();
+  addHoverElement();
+}
+leftExpanderPlugin();
