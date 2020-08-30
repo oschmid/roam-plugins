bfe08992e0ffdd750aa2122ab88d7eb4b7149f77
Oliver Schmid <oliver.schmd@gmail.com>
2020-08-30 10:14:15 -0700

    Namespace

--- random-page-plugin.js
+++ random-page-plugin.js
@@ -1,78 +1,81 @@
-// settings
-const title = 'RANDOM';
-const icon = 'bp3-icon bp3-icon-random icon';
-const shortcut = 82; // Alt+R
+function randomPagePlugin() {
+  // settings
+  const title = 'RANDOM';
+  const icon = 'bp3-icon bp3-icon-random icon';
+  const shortcut = 82; // Alt+R
 
-function addButton() {
-  // cleanup old versions of the button
-  var randomButton = document.querySelector('#random-button');
-  if (randomButton != null) {
-    randomButton.parentNode.removeChild(randomButton);
+  function addButton() {
+    // cleanup old versions of the button
+    var randomButton = document.querySelector('#random-button');
+    if (randomButton != null) {
+      randomButton.parentNode.removeChild(randomButton);
+    }
+    // create button
+    var template = document.createElement('template');
+    template.innerHTML = '<div id="random-button" class="log-button" href="#"><div class="flex-h-box" style="align-items:center;justify-content:space-between;"><span class="' + icon + '"></span>' + title + '</div></div>';
+    template.content.firstChild.onclick = goToRandomPage;
+    randomButton = template.content.firstChild;
+
+    // insert button into sidebar
+    const starred = document.querySelector('.starred-pages-wrapper');
+    document.querySelector('.roam-sidebar-content').insertBefore(randomButton, starred);
   }
-  // create button
-  var template = document.createElement('template');
-  template.innerHTML = '<div id="random-button" class="log-button" href="#"><div class="flex-h-box" style="align-items:center;justify-content:space-between;"><span class="' + icon + '"></span>' + title + '</div></div>';
-  template.content.firstChild.onclick = goToRandomPage;
-  randomButton = template.content.firstChild;
-  
-  // insert button into sidebar
-  const starred = document.querySelector('.starred-pages-wrapper');
-  document.querySelector('.roam-sidebar-content').insertBefore(randomButton, starred);
-}
 
-function addKeyboardShortcut() {
-  document.onkeyup = function(e) {
-    var key = e.which || e.keyCode;
-    if (e.altKey && key === shortcut) {
-      goToRandomPage(e);
+  function addKeyboardShortcut() {
+    document.onkeyup = function(e) {
+      var key = e.which || e.keyCode;
+      if (e.altKey && key === shortcut) {
+        goToRandomPage(e);
+      }
     }
   }
-}
 
-function goToRandomPage(e) {
-  if (isAllPages()) {
-    clickRandomPageLink(e.shiftKey);
-  } else if (e.shiftKey) {
-    goToAllPagesThen(function() {
+  function goToRandomPage(e) {
+    if (isAllPages()) {
       clickRandomPageLink(e.shiftKey);
-      history.back();
-    });
-  } else {
-    const allPages = roamAlphaAPI.q('[ :find (pull ?e [:block/uid]) :where [?e :node/title]]');
-    const page = getRandomElement(allPages);
-    const uid = page[0].uid;
-    const db = location.hash.split('/')[2];
-    location.assign('/#/app/' + db + '/page/' + uid);
-  }
-}
-
-function isAllPages() {
-  return location.hash.endsWith('/search');
-}
-
-function goToAllPagesThen(f) {
-  document.querySelector('.bp3-icon-list').parentNode.parentNode.click();
-  setTimeout(f, 0);
-}
-
-function clickRandomPageLink(shift) {
-  // https://forum.roamresearch.com/t/what-would-be-your-top-3-tips-for-beginners/255/9
-  var allPages = document.querySelectorAll('div.rm-pages-title-col a');
-  var pageLink = getRandomElement(allPages);
-  getEventHandlers(pageLink).onClick({ shiftKey: shift });
-}
-
-function getRandomElement(array) {
-  return array[Math.floor(Math.random() * array.length)];
-}
-
-function getEventHandlers(element) {
-  for (var prop in element) {
-    if (prop.includes('reactEventHandlers')) {
-      return element[prop];
+    } else if (e.shiftKey) {
+      goToAllPagesThen(function() {
+        clickRandomPageLink(e.shiftKey);
+        history.back();
+      });
+    } else {
+      const allPages = roamAlphaAPI.q('[ :find (pull ?e [:block/uid]) :where [?e :node/title]]');
+      const page = getRandomElement(allPages);
+      const uid = page[0].uid;
+      const db = location.hash.split('/')[2];
+      location.assign('/#/app/' + db + '/page/' + uid);
     }
   }
-}
 
-addButton();
-addKeyboardShortcut();
+  function isAllPages() {
+    return location.hash.endsWith('/search');
+  }
+
+  function goToAllPagesThen(f) {
+    document.querySelector('.bp3-icon-list').parentNode.parentNode.click();
+    setTimeout(f, 0);
+  }
+
+  function clickRandomPageLink(shift) {
+    // https://forum.roamresearch.com/t/what-would-be-your-top-3-tips-for-beginners/255/9
+    var allPages = document.querySelectorAll('div.rm-pages-title-col a');
+    var pageLink = getRandomElement(allPages);
+    getEventHandlers(pageLink).onClick({ shiftKey: shift });
+  }
+
+  function getRandomElement(array) {
+    return array[Math.floor(Math.random() * array.length)];
+  }
+
+  function getEventHandlers(element) {
+    for (var prop in element) {
+      if (prop.includes('reactEventHandlers')) {
+        return element[prop];
+      }
+    }
+  }
+
+  addButton();
+  addKeyboardShortcut();
+}
+randomPagePlugin();
