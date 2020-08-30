9e662f215a5ef9164f07cd19522d33d4a7e51e06
Oliver Schmid <oliver.schmd@gmail.com>
2020-08-30 10:30:26 -0700

    Create clear-sidebar-plugin.js

---
+++ clear-sidebar-plugin.js
@@ -0,0 +1,38 @@
+function clearSidebarPlugin() {
+  // settings
+  const shortcut = 67; // Alt+C
+  
+  function addButton() {
+    // cleanup old versions of the button
+    var clearButton = document.querySelector('#clear-button');
+    if (clearButton != null) {
+      clearButton.parentNode.removeChild(clearButton);
+    }
+    // create button
+    var template = document.createElement('template');
+    template.innerHTML = '<button id="clear-button" class="bp3-button bp3-minimal bp3-small bp3-icon-standard bp3-icon-cross"></button>';
+    template.content.firstChild.onclick = clearSidebar;
+    clearButton = template.content.firstChild;
+    
+    // insert button into sidebar
+    const sidebarMenu = document.querySelector('#right-sidebar .flex-h-box');
+    sidebarMenu.appendChild(clearButton);
+  }
+
+  function addKeyboardShortcut() {
+    document.onkeyup = function(e) {
+      var key = e.which || e.keyCode;
+      if (e.altKey && key === shortcut) {
+        clearSidebar();
+      }
+    }
+  }
+
+  function clearSidebar() {
+    document.querySelectorAll('.bp3-icon-cross').forEach(e => e.click());
+  }
+
+  addButton();
+  addKeyboardShortcut();
+}
+clearSidebarPlugin();
